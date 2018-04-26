fd9218df203f53b8534d1620db548b18a9cc29cd
jeremy farmer <jeremy.farmer@dhgllp.com>
2018-04-26 09:35:59 -0400

    Adding welcomepage middleware

--- ASPCore_jf/ASPCore_jf/IGreeter.cs
+++ ASPCore_jf/ASPCore_jf/IGreeter.cs
@@ -1,6 +1,28 @@
-﻿namespace ASPCore_jf
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ASPCore_jf
 {
     public interface IGreeter
     {
+        string GetMessageOfTheDay();
+
+
+    }
+
+
+    public class Greeter : IGreeter
+    {
+        private IConfiguration _configuration;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        public string GetMessageOfTheDay()
+        {
+            return _configuration["Greeting"];
+        }
     }
 }
--- ASPCore_jf/ASPCore_jf/Startup.cs
+++ ASPCore_jf/ASPCore_jf/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ASPCore_jf
@@ -15,20 +16,27 @@ namespace ASPCore_jf
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, 
-                              IHostingEnvironment env)
+                              IHostingEnvironment env,
+                              IGreeter greeter)
         {
-            if (env.IsDevelopment())
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            app.UseWelcomePage(new WelcomePageOptions
             {
-                app.UseDeveloperExceptionPage();
-            }
+                Path = "/wp"
+            });
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var greeting = greeter.GetMessageOfTheDay();
+                await context.Response.WriteAsync(greeting);
             });
         }
     }
