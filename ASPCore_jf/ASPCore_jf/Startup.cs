150c67d450c4d3f21d66e2764ae60351db3e7713
jeremy farmer <jeremy.farmer@dhgllp.com>
2018-04-26 08:55:39 -0400

    Add json file appsettings

--- ASPCore_jf/ASPCore_jf/Startup.cs
+++ ASPCore_jf/ASPCore_jf/Startup.cs
@@ -18,7 +18,8 @@ namespace ASPCore_jf
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, 
+                              IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
